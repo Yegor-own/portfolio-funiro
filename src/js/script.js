567c018235677f524fd477feb433cad00c2ca79d
Yegor-own <68266911+Yegor-own@users.noreply.github.com>
2021-07-09 18:45:22 +0500

    add spoilers in navbar

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
-@@include('_webp.js')
-@@include('_touch-screen.js')
-@@include('_spollers.js')
+
+"use strict"
+
+window.onload = function () {
+  @@include('_webp.js')
+  @@include('_touch-screen.js')
+  @@include('_spollers.js')
+}
